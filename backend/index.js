3342afb635ed5c1153b14b6299438939e27ed8a3
Gustavo Lima <lima_gus@outlook.com>
2022-03-23 09:21:42 -0300

    feat: add backend files

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const { UPSIDEDOWN_MODE, PORT } = process.env;
+
+const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
+const StrangerThingsRepository = require('./data/repository/StrangerThings');
+const StrangerThingsService = require('./services/StrangerThings');
+
+const app = express();
+
+const strangerThingsRepository = new StrangerThingsRepository(
+  strangerThingsDataset,
+);
+const strangerThingsService = new StrangerThingsService(
+  strangerThingsRepository,
+);
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  const characters = strangerThingsService.search(
+    req.query,
+    UPSIDEDOWN_MODE,
+  );
+
+  res.status(200).json(characters);
+});
+
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
+});
